b456cd85467c7fc49259c3d65945e0801f896b62
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-10 20:08:21 +0300

    filter: template base

--- src/js/pages/homePage.js
+++ src/js/pages/homePage.js
@@ -1,4 +1,8 @@
+import filter from './../filter/filterController';
+
 export default function () {
-  const markup = `<div class="container"><h1>Home page</h1></div>`;
   document.querySelector('#app').innerHTML = markup;
+
+  filter();
 }
+
