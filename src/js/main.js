96458fb36069ecc40988f257894d82208ba3ffb9
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-10 18:45:44 +0300

    base: router base logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
-console.log('Hello world!')
+import homePage from './pages/homePage';
+import singleItem from './pages/singleItemPage';
+import favouritesPage from './pages/favouritesPage';
+import bidsPage from './pages/bidsPage';
+import errorPage from './pages/errorPage';
+
+
+// Routes
+const routes = [
+  {path: '/', component: homePage},
+  {path: 'item', component: singleItem},
+  {path: 'favourites', component: favouritesPage},
+  {path: 'bids', component: bidsPage}
+];
+
+// find component in routes by route
+const findComponentByPath = function (path, routes) {
+  return routes.find( function (route) {
+    return route.path === path; //  return route we search for
+  });
+}
+
+// Router 
+const router = function () {
+  // Split path to array
+  const pathArray = location.hash.split('/');
+
+  // Set current path
+  let currentPath = pathArray[0] === '' ? '/' : pathArray[1]; // no params - main page
+  currentPath = currentPath === '' ? '/' : currentPath; 
+
+  // Current matching Component from router or error page
+  const { component = errorPage } = findComponentByPath(currentPath, routes) || {};
+
+  component();
+}
+
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);
---
+++ src/js/pages/homePage.js
@@ -0,0 +1,4 @@
+export default function () {
+  const markup = `<div class="container"><h1>Home page</h1></div>`;
+  document.querySelector('#app').innerHTML = markup;
+}
