18046bfb8d13842cfa8b19fdcd01e623837857b1
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-11 15:55:43 +0300

    component singleItem: render card without data

--- src/js/singleItem/singleItemController.js
+++ src/js/singleItem/singleItemController.js
@@ -1,8 +1,12 @@
 import SingleItem from './singleItemModel';
+import * as view from './singleItemView';
 
 export default function (state) {
   console.log('single item contoller start');
   
   state.singleItem = new SingleItem(state.routeParams);
   state.singleItem.getItem();
+
+  // Render single card 
+  view.render();
 }
