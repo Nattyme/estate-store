fec3f7008d76aacc5b97da1aedc431219ac41e30
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-11 18:33:29 +0300

    component singleItem: modal open and close by click to button, close by click to overlay

--- src/js/singleItem/singleItemController.js
+++ src/js/singleItem/singleItemController.js
@@ -9,4 +9,25 @@ export default async function (state) {
 
   // Render single card 
   view.render(state.singleItem.result);
+
+  /* **********
+  * Start event listen
+  ************ */
+
+  // Open modal
+  document.querySelector('.button-order').addEventListener('click', ()=>{
+    view.showModal();    
+  });
+
+  // Close modal if 'buttons-close' is clicked
+  document.querySelector('.modal__close').addEventListener('click', ()=>{
+    view.hideModal();    
+  });
+
+  // Clode mode if overlay is clicked
+  document.querySelector('.modal-wrapper').addEventListener('click', (e)=>{
+    if ( !e.target.closest('.modal')) {
+      view.hideModal();    
+    };
+  });
 }
