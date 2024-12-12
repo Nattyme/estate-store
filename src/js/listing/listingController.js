669d57f5a688126e0d7f41517bd31f22f08879c8
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-12 15:42:50 +0300

    component listing: favs icons display correction stat after filter

--- src/js/listing/listingController.js
+++ src/js/listing/listingController.js
@@ -1,6 +1,24 @@
 import * as view from './listingView';
 
 export default function (state) {
+   // Function works with 'add to favs' icons
+  const  addToFavListener = function () {
+    Array.from( document.getElementsByClassName('card__like')).forEach( (item)=>{
+      item.addEventListener('click', (e)=> {
+        e.preventDefault();
+  
+        // Finde id of a clicked obj
+        const currentId = e.target.closest('.card').dataset.id;
+        
+        // Add or remove fav in storage
+        state.favourites.toggleFav(currentId);
+        
+        // Turn fav icon on or off
+        view.toggleFavIcon(e.target.closest('.card__like'), state.favourites.isFav(currentId));
+        
+      });
+    });
+  }
 
   // Cards container render
   view.render();
@@ -10,14 +28,18 @@ export default function (state) {
     view.renderCard(item, state.favourites.isFav(item.id));
   });
 
+
   state.emitter.subscribe('event:render-listing', ()=>{
     // Clear cords container
     view.clearListingContainer();
 
     // Cards render
     state.results.forEach( (item) => {
-      view.renderCard(item);
+      view.renderCard(item, state.favourites.isFav(item.id));
     });
+
+    // Start listen click at 'add to fav' icon
+    addToFavListener();
   });
 
   state.emitter.subscribe('event:reset-listing', ()=> {
@@ -26,26 +48,16 @@ export default function (state) {
 
     // Cards render
     state.results.forEach( (item) => {
-      view.renderCard(item);
+      view.renderCard(item, state.favourites.isFav(item.id));
     });
 
-  });
+    // Start listen click at 'add to fav' icon
+    addToFavListener();
 
-  Array.from( document.getElementsByClassName('card__like')).forEach( (item)=>{
-    item.addEventListener('click', (e)=> {
-      e.preventDefault();
-      console.log('clicked fav');
-      
-      // Finde id of a clicked obj
-      const currentId = e.target.closest('.card').dataset.id;
-      
-      // Add or remove fav in storage
-      state.favourites.toggleFav(currentId);
-      
-      // Turn fav icon on or off
-      view.toggleFavIcon(e.target.closest('.card__like'), state.favourites.isFav(currentId));
-      
-    });
-  });
+  }); 
+
+ 
+
+  
   
 }
